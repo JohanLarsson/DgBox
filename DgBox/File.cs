72253eb9f384d9c065044aabc52c5e95136165db
Johan Larsson <johan_gavle1@hotmail.com>
2014-07-23 15:58:28 +0200

    Added checkbox

--- DgBox/File.cs
+++ DgBox/File.cs
@@ -1,13 +1,51 @@
 ﻿namespace DgBox
 {
-    public class File
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    using DgBox.Annotations;
+
+    public class File : INotifyPropertyChanged
     {
+        private bool active;
+
         public File(string fileName, string localFilePath)
         {
             this.FileName = fileName;
             this.LocalFilePath = localFilePath;
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public string FileName { get; private set; }
+
         public string LocalFilePath { get; private set; }
+
+        public bool Active
+        {
+            get
+            {
+                return this.active;
+            }
+            set
+            {
+                if (value.Equals(this.active))
+                {
+                    return;
+                }
+                this.active = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
