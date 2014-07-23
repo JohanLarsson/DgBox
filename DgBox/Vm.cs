201f6c7fc4891bfa84198e37a4607ba7dd88852b
Johan Larsson <johan_gavle1@hotmail.com>
2014-07-23 16:41:03 +0200

    With add button

--- DgBox/Vm.cs
+++ DgBox/Vm.cs
@@ -1,15 +1,44 @@
 ﻿namespace DgBox
 {
     using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using System.Windows.Input;
 
-    public class Vm
+    using DgBox.Annotations;
+
+    public class Vm : INotifyPropertyChanged
     {
         private readonly ObservableCollection<File> files = new ObservableCollection<File>();
+
+        private string newFile;
 
         public Vm()
         {
             Files.Add(new File("1", @"C:\XXX"));
             Files.Add(new File("2", @"C:\XXX"));
+            AddCommand = new RelayCommand(
+                _ => Files.Add(new File(NewFile, @"C:\XXX")),
+                _ => !string.IsNullOrEmpty(NewFile));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string NewFile
+        {
+            get
+            {
+                return this.newFile;
+            }
+            set
+            {
+                if (value == this.newFile)
+                {
+                    return;
+                }
+                this.newFile = value;
+                this.OnPropertyChanged();
+            }
         }
 
         public ObservableCollection<File> Files
@@ -17,6 +46,18 @@
             get
             {
                 return this.files;
+            }
+        }
+
+        public ICommand AddCommand { get; private set; }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
             }
         }
     }
