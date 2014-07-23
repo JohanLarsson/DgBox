90960ee4474b74d2ac48c1aca7b976f4672c8900
Johan Larsson <johan_gavle1@hotmail.com>
2014-07-23 15:54:59 +0200

    Initial

---
+++ DgBox/File.cs
@@ -0,0 +1,13 @@
+﻿namespace DgBox
+{
+    public class File
+    {
+        public File(string fileName, string localFilePath)
+        {
+            this.FileName = fileName;
+            this.LocalFilePath = localFilePath;
+        }
+        public string FileName { get; private set; }
+        public string LocalFilePath { get; private set; }
+    }
+}
---
+++ DgBox/Vm.cs
@@ -0,0 +1,23 @@
+﻿namespace DgBox
+{
+    using System.Collections.ObjectModel;
+
+    public class Vm
+    {
+        private readonly ObservableCollection<File> files = new ObservableCollection<File>();
+
+        public Vm()
+        {
+            Files.Add(new File("1", @"C:\XXX"));
+            Files.Add(new File("2", @"C:\XXX"));
+        }
+
+        public ObservableCollection<File> Files
+        {
+            get
+            {
+                return this.files;
+            }
+        }
+    }
+}
